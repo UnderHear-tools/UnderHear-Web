15ca691494541f2918d5d1d3563bc246d677ed21
UnderHear <2533643340@qq.com>
2025-12-14 20:33:16 +0800

    更改Monaco配置文件位置

--- UnderHear-frontend/src/main.ts
+++ UnderHear-frontend/src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-import '@/monaco/setupMonaco'
+import '@/lib/monaco/setupMonaco'
 
 const app = createApp(App)
 app.use(router)
