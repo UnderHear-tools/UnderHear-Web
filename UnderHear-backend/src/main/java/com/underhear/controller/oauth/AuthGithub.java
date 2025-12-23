4860335930c96708e5f48be8e37996bd9467ddc4
UnderHear <2533643340@qq.com>
2025-12-24 00:48:03 +0800

    重构后端

---
+++ UnderHear-backend/src/main/java/com/underhear/controller/oauth/AuthGithub.java
@@ -0,0 +1,48 @@
+package com.underhear.controller.oauth;
+
+import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.request.AuthGithubRequest;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthStateUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@RestController
+@RequestMapping("/oauth")
+public class AuthGithub {
+
+    @Value("${github.oauth.client-id}")
+    private String clientId;
+
+    @Value("${github.oauth.client-secret}")
+    private String clientSecret;
+
+    @Value("${github.oauth.redirect-uri}")
+    private String redirectUri;
+
+    @RequestMapping("/render")
+    public void renderAuth(HttpServletResponse response) throws IOException {
+        AuthRequest authRequest = getAuthRequest();
+        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+    }
+
+    @RequestMapping("/callback")
+    public Object login(AuthCallback callback) {
+        AuthRequest authRequest = getAuthRequest();
+        return authRequest.login(callback);
+    }
+
+    private AuthRequest getAuthRequest() {
+        return new AuthGithubRequest(AuthConfig.builder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .redirectUri(redirectUri)
+                .build());
+    }
+}
--- UnderHear-backend/src/main/java/com/underhear/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.underhear.service;
-
-import com.underhear.dto.AuthResponse;
-
-public interface AuthService {
-
-    AuthResponse authenticateWithGithubCode(String code, String state);
-}
-
