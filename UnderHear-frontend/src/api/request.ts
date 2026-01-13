09b455b97fb4da0a64a19f65999a3ffec6a080cc
UnderHear <2533643340@qq.com>
2026-01-14 00:54:41 +0800

    封装统一请求

---
+++ UnderHear-frontend/src/api/request.ts
@@ -0,0 +1,42 @@
+import axios, { type AxiosRequestConfig } from 'axios'
+
+import { API_BASE_URL } from './config'
+
+const request = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 15000,
+})
+
+request.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (axios.isAxiosError(error)) {
+      const responseData = error.response?.data as { message?: string } | undefined
+      const message = responseData?.message ?? error.message
+      return Promise.reject(new Error(message))
+    }
+    return Promise.reject(error)
+  }
+)
+
+export const setAuthToken = (token?: string) => {
+  if (token) {
+    request.defaults.headers.common.Authorization = `Bearer ${token}`
+    return
+  }
+  delete request.defaults.headers.common.Authorization
+}
+
+export const get = <T>(url: string, config?: AxiosRequestConfig) =>
+  request.get<T, T>(url, config)
+
+export const post = <T>(url: string, data?: unknown, config?: AxiosRequestConfig) =>
+  request.post<T, T>(url, data, config)
+
+export const put = <T>(url: string, data?: unknown, config?: AxiosRequestConfig) =>
+  request.put<T, T>(url, data, config)
+
+export const del = <T>(url: string, config?: AxiosRequestConfig) =>
+  request.delete<T, T>(url, config)
+
+export default request
