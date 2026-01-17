44f481c4a340ca92c5e391356ccd87fe94e55eb5
西江月 <2533643340@qq.com>
2026-01-17 17:17:37 +0800

    修改描述语

--- UnderHear-frontend/src/module/application/views/application.vue
+++ UnderHear-frontend/src/module/application/views/application.vue
@@ -1,9 +1,9 @@
 <template>
   <zContainer>
     <div class="application-header">
-      <h1 class="application-title">单页应用</h1>
+      <h1 class="application-title">Web应用</h1>
       <p class="application-subtitle">从一个小创意开始🐱。</p>
-      <p class="application-subtitle">想上传自己的单页应用吗？请<zLink href="/application/create" link-text="点击这里！" />欢迎大家上传！</p>
+      <p class="application-subtitle">想上传自己的Web应用吗？请<zLink href="/application/create" link-text="点击这里！" />欢迎大家上传！</p>
       <p class="application-subtitle">希望这些应用能够帮到你。</p>
     </div>
 
