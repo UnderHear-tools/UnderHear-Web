2d46650a1171b934fe6cb907dbe8837aadb67239
UnderHear <2533643340@qq.com>
2026-01-19 23:29:09 +0800

    新增Octicons-vue图标库

---
+++ UnderHear-frontend/src/components/z-ui/icon/Octicons-vue/index.ts
@@ -0,0 +1,1 @@
+export { default as accessibility } from "./icons/accessibility.vue"
--- UnderHear-frontend/src/module/index/views/index.vue
+++ UnderHear-frontend/src/module/index/views/index.vue
@@ -2,11 +2,13 @@
   <zContainer>
     <h1>Welcome to UnderHear Frontend</h1>
     <p>This is the main index view of the UnderHear frontend application.</p>
+    <accessibility :size="24" />
   </zContainer>
 </template>
 
 <script setup lang="ts">
 import { zContainer } from '@/components/z-ui/container/zContainer'
+import { accessibility } from '@/components/z-ui/icon/Octicons-vue/index.ts'
 </script>
 
 <style scoped>
