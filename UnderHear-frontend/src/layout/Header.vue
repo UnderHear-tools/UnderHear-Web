3f7089ab688a115c2aa16ce1f7102fe0a2bf08d6
UnderHear <2533643340@qq.com>
2026-02-02 01:33:04 +0800

    完善layout中的user-menu

--- UnderHear-frontend/src/layout/Header.vue
+++ UnderHear-frontend/src/layout/Header.vue
@@ -12,7 +12,8 @@
       </nav>
 
       <div class="user-menu">
-        <zDropdown placement="left-bottom">
+        <a v-if="true" class="nav-link" href="/auth/login">登录</a>
+        <zDropdown v-if="false" placement="left-bottom">
           <template #trigger>
             <zAvatar src="https://avatars.githubusercontent.com/u/131276691?v=4" :size="32" />
           </template>
