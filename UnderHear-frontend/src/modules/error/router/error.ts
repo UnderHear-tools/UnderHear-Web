42dd454df6198a3bae59db360d088c80e1b8a630
UnderHear <2533643340@qq.com>
2026-02-22 01:39:07 +0800

    新增调试场

--- UnderHear-frontend/src/modules/error/router/error.ts
+++ UnderHear-frontend/src/modules/error/router/error.ts
@@ -5,5 +5,10 @@ export const errorRoutes: Array<RouteRecordRaw> = [
         path: '/:pathMatch(.*)*',
         name: 'NotFound',
         component: () => import('../views/404.vue')
+    },
+    {
+        path: '/playground',
+        name: 'Playground',
+        component: () => import('../views/playground.vue')
     }
 ]
