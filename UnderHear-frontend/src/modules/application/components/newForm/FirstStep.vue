ee2c7f9f92c8c0b3203051fee1d6e4ceeb526630
UnderHear <2533643340@qq.com>
2026-03-16 21:03:02 +0800

    一些小的质量更新

--- UnderHear-frontend/src/modules/application/components/newForm/FirstStep.vue
+++ UnderHear-frontend/src/modules/application/components/newForm/FirstStep.vue
@@ -72,19 +72,10 @@
     </div>
     <p
       v-if="invalid && validation"
-      class="step-validation"
-      role="alert"
     >
-      <svg
-        aria-hidden="true"
-        focusable="false"
-        width="12"
-        height="12"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        class="icon-svg"
-      ><path d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575ZM8 5a.75.75 0 0 0-.75.75v2.5a.75.75 0 0 0 1.5 0v-2.5A.75.75 0 0 0 8 5Zm1 6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z" /></svg>
-      {{ validation }}
+      <zFormControlValidation variant="error">
+        {{ validation }}
+      </zFormControlValidation>
     </p>
   </div>
 </template>
@@ -92,6 +83,7 @@
 <script setup lang="ts">
 import { zTag } from '@/components/z-ui/tag'
 import type { FrameworkValue } from './useCreateApplicationForm'
+import { zFormControlValidation } from '@/components/z-ui/form-control'
 
 interface Props {
   selectedFramework: FrameworkValue | null
@@ -167,14 +159,6 @@ function selectFramework(framework: FrameworkValue) {
   width: 50%;
   height: auto;
   margin-top: 0.5rem;
-}
-
-.step-validation {
-  display: flex;
-  font-size: 12px;
-  line-height: 1.3333333333;
-  font-weight: 600;
-  color: var(--fgColor-danger, #d1242f);
 }
 
 .icon-svg {
