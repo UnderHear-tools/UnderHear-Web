2aa6d4376a0f0f59e31441e54425955b9c20b4da
UnderHear <2533643340@qq.com>
2026-03-23 01:34:39 +0800

    增加application实体类

---
+++ UnderHear-backend/src/main/java/com/underhear/pojo/entity/Application.java
@@ -0,0 +1,23 @@
+package com.underhear.pojo.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Application {
+    private Long id;
+    private String appid;
+    private String ownerUuid;
+    private String framework;
+    private String appName;
+    private String appEnglishName;
+    private String visibility;
+    private String appDescription;
+    private String storagePath;
+    private String originalFilename;
+    private String contentType;
+    private Double fileSize;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
