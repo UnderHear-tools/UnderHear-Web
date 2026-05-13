14fda310d6b46e9e84d3098cfc11704119aff922
UnderHear <2533643340@qq.com>
2026-05-13 23:05:09 +0800

    去除不存在的路由

--- UnderHear-frontend/src/modules/application/router/application.ts
+++ UnderHear-frontend/src/modules/application/router/application.ts
@@ -30,10 +30,5 @@ export const applicationRoutes: Array<RouteRecordRaw> = [
         path: '/application',
         name: 'Application',
         component: () => import('../views/application.vue')
-    },
-    {
-        path: '/application/pathplanner',
-        name: 'PathPlanner',
-        component: () => import('../views/application/pathplanner/pathplanner.vue')
     }
 ]
