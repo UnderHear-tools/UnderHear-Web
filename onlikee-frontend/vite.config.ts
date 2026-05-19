fd5687447d3d6858cbace7fe04420eb1878e1dac
UnderHear <2533643340@qq.com>
2026-05-19 23:53:01 +0800

    卸载 rollup-plugin-visualizer

--- onlikee-frontend/vite.config.ts
+++ onlikee-frontend/vite.config.ts
@@ -2,18 +2,18 @@ import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import { visualizer } from 'rollup-plugin-visualizer';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
     vue(), 
     tailwindcss(), 
-    visualizer({
-      open: true,        // 打包完成后，自动在浏览器中打开报告
-      filename: 'packingStatistics.html', // 生成的分析报告文件名
-      gzipSize: true,    // 显示文件经过Gzip压缩后的大小
-      brotliSize: true,  // 显示文件经过Brotli压缩后的大小
-    }),
+    // visualizer({
+    //   open: false,        // 打包完成后，自动在浏览器中打开报告
+    //   filename: 'packingStatistics.html', // 生成的分析报告文件名
+    //   gzipSize: true,    // 显示文件经过Gzip压缩后的大小
+    //   brotliSize: true,  // 显示文件经过Brotli压缩后的大小
+    // }),
   ],
   resolve: {
     alias: {
