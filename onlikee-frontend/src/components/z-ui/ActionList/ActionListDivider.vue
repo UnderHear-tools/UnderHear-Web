fa965d6fe91d213befc63f3af8207681fe823256
UnderHear <2533643340@qq.com>
2026-05-28 21:41:23 +0800

    优化divider样式，确保在不同缩放下不会出现粗细诧异

--- onlikee-frontend/src/components/z-ui/ActionList/ActionListDivider.vue
+++ onlikee-frontend/src/components/z-ui/ActionList/ActionListDivider.vue
@@ -7,12 +7,10 @@
 
 <style scoped>
 .action-list-divider {
-  display: block;
-  height: 1px;
-  padding: 0;
-  margin-block: 7px 8px;
-  list-style: none;
-  background: var(--borderColor-muted, #d1d9e0b3);
   border: 0;
+  border-block-start: 1px solid var(--borderColor-muted, #d1d9e0b3);
+  height: 1px;
+  margin: 0.5rem 0;
+  width: 100%;
 }
 </style>
